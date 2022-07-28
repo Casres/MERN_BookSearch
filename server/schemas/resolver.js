75142245806d724c073cdd87c2d6506befcd8875
Casres <christian@casillas.tech>
2022-07-28 15:23:22 -0400

    Refactored reesolver

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -1,64 +1,66 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id }).select('-__v - password');
-                return userData;
-            }
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v - password"
+        );
+        return userData;
+      }
 
-            throw new AuthenticationError('Not logged in');
-        }
+      throw new AuthenticationError("Not logged in");
     },
+  },
 
-    Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-            return { token, user };
-        },
-        login: async (parent, {email, password}) => {
-            const user = await User.findOne({ email });
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-            if (!user) throw new AuthenticationError('Incorrect credentials!');
+      if (!user) throw new AuthenticationError("Incorrect credentials!");
 
-            const correctPw = await User.isCorrectPassword(password);
+      const correctPw = await User.isCorrectPassword(password);
 
-            if (!correctPw) throw new AuthenticationError('Incorrect password!');
+      if (!correctPw) throw new AuthenticationError("Incorrect password!");
 
-            const token = signToken(user);
+      const token = signToken(user);
 
-            return { token, user };
-        },
-        saveBook: async (parent, {bookData}, context) => {
-            if (context.user) {
-                const updateUser = await User.findIdAndUpdate(
-                    {_id: context.user._id},
-                    {$push: {savedBooks:bookData}},
-                    {new: true}
-                    );
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updateUser = await User.findIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
 
-                return updateUser;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
-        removeBook: async (parent, {bookId}, context) => {
-            if (context.user) {
-                const removeBook = await User.findIdAndUpdate(
-                    {_id: context.user._id},
-                    {$pull: {savedBooks: {bookId}}},
-                    {new: true}        
-                );
+        return updateUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const removeBook = await User.findIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
 
-                return removeBook;
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        }
-    }
-}
+        return removeBook;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
 
 module.exports = resolvers;
